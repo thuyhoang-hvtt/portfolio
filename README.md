f13a54424f11140d57c0148a07d67daaf216521d
0xShikYe <thuyhoang.hvtt@gmail.com>
2022-01-08 16:32:23 +0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  0xShikYe - Portfolio
+</h1>
+
+<div align="center">
+  <img alt="Logo" src="https://raw.githubusercontent.com/portfolio/main/src/images/wip.png" width="100" />
+</div>
---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.thuyhoang.hvtt",
+    title: "Thuy Hoang",
+    description: "Thuy Hoang is a software engineer who owns a passionate interest in new technologies (Web, AI, Blockchain and Metaverse).",
+    twitterUsername: "thuyhoang_hvtt",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: 'Thuy Hoang',
+        short_name: '0xShikYe',
+        start_url: '/',
+        icon: "src/images/logo.png",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
