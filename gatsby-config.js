4279b5e44e3da8eac3e8f68141632450e8757bfd
0xShikYe <thuyhoang.hvtt@gmail.com>
2022-01-12 18:07:00 +0700

    💥 Use Typescript & Yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,42 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.thuyhoang.hvtt",
-    title: "Thuy Hoang",
-    description: "Thuy Hoang is a software engineer who owns a passionate interest in new technologies (Web, AI, Blockchain and Metaverse).",
-    twitterUsername: "thuyhoang_hvtt",
+    siteUrl: 'https://www.thuyhoang.hvtt',
+    title: 'Thuy Hoang',
+    description:
+      'Thuy Hoang is a software engineer who owns a passionate interest in new technologies (Web, AI, Blockchain and Metaverse).',
+    twitterUsername: 'thuyhoang_hvtt',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Thuy Hoang',
         short_name: '0xShikYe',
         start_url: '/',
-        icon: "src/images/logo@24px.png",
+        icon: 'src/images/logo@24px.png',
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
