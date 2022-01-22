128f62200584779d0f195f4266690428b00846d8
0xShikYe <thuyhoang.hvtt@gmail.com>
2022-01-22 11:29:57 +0700

    💫 Add icons

---
+++ src/components/icons/bookmark.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function IconBookmark() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-bookmark"
+    >
+      <title>Bookmark</title>
+      <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+    </svg>
+  );
+}
+
+export default IconBookmark;
---
+++ src/components/icons/icon.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  IconAppStore,
+  IconBookmark,
+  IconCodepen,
+  IconExternal,
+  IconFolder,
+  IconFork,
+  IconGitHub,
+  IconInstagram,
+  IconLinkedin,
+  IconPlayStore,
+  IconStar,
+  IconTwitter,
+} from '@/components/icons';
+
+interface IProps {
+  name: string;
+}
+
+function Icon(props: IProps) {
+  const { name } = props;
+
+  switch (name) {
+    case 'AppStore':
+      return <IconAppStore />;
+    case 'Bookmark':
+      return <IconBookmark />;
+    case 'Codepen':
+      return <IconCodepen />;
+    case 'External':
+      return <IconExternal />;
+    case 'Folder':
+      return <IconFolder />;
+    case 'Fork':
+      return <IconFork />;
+    case 'GitHub':
+      return <IconGitHub />;
+    case 'Instagram':
+      return <IconInstagram />;
+    case 'Linkedin':
+      return <IconLinkedin />;
+    case 'PlayStore':
+      return <IconPlayStore />;
+    case 'Star':
+      return <IconStar />;
+    case 'Twitter':
+      return <IconTwitter />;
+    default:
+      return <IconExternal />;
+  }
+}
+
+export default Icon;
