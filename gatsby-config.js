81ca4acabe6920b1f3404db8f720a13b1c62f32b
Trọng Thụy <thuyhoang@kms-technology.com>
2022-02-20 10:34:55 +0700

    🔧 Config gatsby to use remark transform

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,36 @@ module.exports = {
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          {
+            // https://www.gatsbyjs.org/packages/gatsby-remark-images
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 700,
+              linkImagesToOriginal: true,
+              quality: 90,
+              tracedSVG: { color: '#b817e3' },
+            },
+          },
+          {
+            // https://www.gatsbyjs.org/packages/gatsby-remark-code-titles/
+            resolve: 'gatsby-remark-code-titles',
+          }, // IMPORTANT: this must be ahead of other plugins that use code blocks
+        ],
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -27,18 +57,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'images',
-        path: './src/images/',
+        name: 'content',
+        path: `${__dirname}/content/`,
       },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/',
-      },
-      __key: 'pages',
     },
   ],
 };
